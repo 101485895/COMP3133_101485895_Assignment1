a51c2ce2b03c3f8afae45d011e634a0a7fd99e4d
101485895 <101485895@georgebrown.ca>
2026-04-11 15:53:28 -0400

    made email and gender required for consistency

--- src/models/Employee.js
+++ src/models/Employee.js
@@ -4,10 +4,11 @@ const employeeSchema = new mongoose.Schema(
   {
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
-    email: { type: String, unique: true },
+    email: { type: String, required: true, unique: true },
     gender: {
       type: String,
       enum: ["Male", "Female", "Other"],
+      required: true
     },
     designation: { type: String, required: true },
     salary: {
@@ -17,7 +18,7 @@ const employeeSchema = new mongoose.Schema(
     },
     date_of_joining: { type: Date, required: true },
     department: { type: String, required: true },
-    employee_photo: { type: String }, // will add cloudinary url later
+    employee_photo: { type: String },
   },
   { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
